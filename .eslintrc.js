9a20a89bd92169a33e1a17621aa2363b2c165ba9
jason_gao <jason_gao@mail.chungyo.net>
2023-02-02 14:20:52 +0800

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,63 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:react/recommended',
+    'react-app',
+    'react-app/jest',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx'],
+      },
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: [
+    'react',
+    'jsx-a11y',
+    'import',
+  ],
+  ignorePatterns: ['config', 'public', 'scripts'],
+  rules: {
+    'class-methods-use-this': ['error', {
+      exceptMethods: [
+        'render',
+        'getInitialState',
+        'getDefaultProps',
+        'componentWillMount',
+        'componentDidMount',
+        'componentWillReceiveProps',
+        'shouldComponentUpdate',
+        'componentWillUpdate',
+        'componentDidUpdate',
+        'componentWillUnmount',
+      ],
+    }],
+    'global-require': 0,
+    'no-control-regex': 0,
+    'import/no-named-as-default': 0,
+    'max-len': ['error', { code: 120 }],
+    'react/display-name': 0,
+    'no-plusplus': 0,
+  },
+};
